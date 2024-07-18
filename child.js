7a6e6420eb21da5c34901d7ea8313464effa0d9d
Re12345678 <rehabshaban245@gmail.com>
2024-07-18 04:18:25 +0300

    new commit

---
+++ child.js
@@ -0,0 +1,21 @@
+
+const initApp = () => {
+    // get data product
+    fetch('child.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+        // get data cart from memory
+        if(localStorage.getItem('cart')){
+            cart = JSON.parse(localStorage.getItem('cart'));
+            addCartToHTML();
+        }
+    })
+}
+initApp();
+//search filter
+
+
+
+
